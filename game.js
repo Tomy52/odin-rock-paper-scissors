22ba748c4398763945100ed14220f01372cc0a9a
Tomy52 <toto9116@gmail.com>
2022-07-27 18:43:02 -0300

    Create a .js file for the game's code

---
+++ game.js
@@ -0,0 +1,24 @@
+//Create an array with all the possible choices
+
+let choices = ["rock","paper","scissors"]
+
+//Create two variables for points, one for the player and another for the cpu. Initial value will always be zero
+
+let playerPoints = 0
+let cpuPoints = 0
+
+//Create a function to let the computer choose one option randomly
+
+soon
+
+//Create a function to start the game and let the player choose one option
+
+function play_rps(choice){
+
+}
+
+//Use the function created previously to let the cpu make a choice
+//Compare them
+//Give 1 point to whoever chose the superior option (if they're equal it's a draw and no points will be given)
+//Repeat until round is finished (3 points)
+
