664b0a15b1409a329a1441a98deabd303f179971
Tomy52 <toto9116@gmail.com>
2022-08-14 00:44:36 -0300

    Refactor script and improve website

--- game.js
+++ game.js
@@ -19,86 +19,10 @@ function getComputerChoice(){
     computerChoice = (randomElement);
 }
 
-//Create a function to start the game and let the player choose one option
-
-function getPlayerChoice(){
-    playerChoice = window.prompt("Rock, paper, scissors?");
-}
-
-function isValidChoice(choice){ //Unused
-    return possibleChoices.indexOf(choice) != -1 
-}
-
-//Use the function created previously to let the cpu make a choice
-//Compare them
-
-function compareChoices(computerChoice,playerChoice){
-    if (computerChoice === "rock" && playerChoice === "scissors" ||
-        computerChoice === "paper" && playerChoice === "rock" ||
-        computerChoice === "scissors" && playerChoice === "paper") {
-        cpuPoints += 1
-        return "You lose. " + capitalize(computerChoice) + " beats " + capitalize(playerChoice) + "."
-    } else if (computerChoice === playerChoice) {
-        return "It's a draw. Computer chose " + capitalize(computerChoice) + "."
-    } else {
-        playerPoints += 1
-        return "You win. " + capitalize(playerChoice) + " beats "  + capitalize(computerChoice) + "."
-    }
-}
-
-//Repeat until round is finished (5 points)
-
-
-//Test function to see if it's possible to gather data from a prompt (It is lul)
-function promptTest(){
-    if ((window.prompt("This is a test, do you know that?")) === 'yes') {
-        return "great!"
-    } else {
-        return "this is not supposed to happen"
-    }
-}
-
-//Add a function for playing the game
-
-function play_rps(){
-    getComputerChoice();
-    getPlayerChoice();
-    return compareChoices(computerChoice,playerChoice.toLowerCase());
-}
-
-//Add a function for playing the game for more than one round
-
-function game(rounds){
-    for (let roundsPlayed = 0; roundsPlayed < rounds; roundsPlayed++)  {
-       console.log(play_rps())
-    }
-    if (playerPoints < cpuPoints) {
-        console.log("\n" + checkScore() + "\n ")
-        resetScore()
-        return "Game over bro." 
-    } else if (playerPoints === cpuPoints) {
-        console.log("\n" + checkScore() + "\n ")
-        resetScore()
-        return "Nobody won."
-    } else {
-        console.log("\n" + checkScore() + "\n ")
-        resetScore()
-        return "You win."
-    }
-
-}
-
 //Add a function to check final score at the end of a game
 
 function checkScore(){
     return "The final score is " + playerPoints + "-" + cpuPoints
-}
-
-//Add a function to reset the score when the game finishes
-
-function resetScore(){
-    cpuPoints = 0;
-    playerPoints = 0;
 }
 
 //Add a function to capitalize a string
@@ -109,34 +33,98 @@ function capitalize(text) {
 
 //New approach (UI Based)
 
-//Let the player choose the number of rounds in a text box after "button-start"
-//is pressed
+const choiceButtons = document.querySelector('.choice-buttons')
 
-const roundSelector = document.querySelector('.round-selection')
+const scoreboard = document.querySelector('.scoreboard')
+const score = document.querySelector('#score')
+const result = document.querySelector('#result')
 
 const play = document.querySelector('#button-start')
 play.addEventListener('click', () => {
-    roundSelector.style.visibility = 'visible'
+    play.style.display = 'none'
+    choiceButtons.style.display = 'inline'
+    scoreboard.style.display = 'block'
+    score.textContent = `Current score is ${playerPoints} - ${cpuPoints}.`
 });
 
-//Gather user input and use it on game() function
+function resetScore_new(){
+    cpuPoints = 0;
+    playerPoints = 0;
+    score.textContent = ''
+    play.style.display = 'inline'
+    resetScoreBtn.style.display = 'none'
+}
 
-const roundSelectorBox = document.querySelector('.round-selector')
+const resetScoreBtn = document.querySelector('#button-reset')
+resetScoreBtn.addEventListener('click', () => {
+    resetScore_new()
+});
 
-const roundSelectorBtn = document.querySelector('#select-rounds')
+const rock = document.querySelector('#button-rock')
+const paper = document.querySelector('#button-paper')
+const scissors = document.querySelector('#button-scissors')
 
-let rounds;
+rock.addEventListener('click', () => {
+        console.log(play_rps_new('rock'));
+    });
+    
+paper.addEventListener('click', () => {
+        console.log(play_rps_new('paper'));
+    });
+    
+scissors.addEventListener('click', () => {
+        console.log(play_rps_new('scissors'));
+    });
 
-roundSelectorBtn.addEventListener('click', () => {
-    rounds = Number(roundSelectorBox.value)
-    game(rounds)
-})
 
-//Rework game() function so it doesn't show a prompt asking the user
-//to choose an option and instead show 3 buttons on the page
+function compareChoices_new(computerChoice,playerChoice){
+    if (computerChoice === "rock" && playerChoice === "scissors" ||
+        computerChoice === "paper" && playerChoice === "rock" ||
+        computerChoice === "scissors" && playerChoice === "paper") {
+        cpuPoints += 1
+        score.textContent = `Current score is ${playerPoints} - ${cpuPoints}.`
+        result.style.color = 'red'
+        result.textContent = "You lose. " + capitalize(computerChoice) + " beats " + capitalize(playerChoice) + "."
+    } else if (computerChoice === playerChoice) {
+        score.textContent = `Current score is ${playerPoints} - ${cpuPoints}.`
+        result.style.color = 'grey'
+        result.textContent = "It's a draw. Computer chose " + capitalize(computerChoice) + "."
+    } else {
+        playerPoints += 1
+        score.textContent = `Current score is ${playerPoints} - ${cpuPoints}.`
+        result.style.color = 'green'
+        result.textContent = "You win. " + capitalize(playerChoice) + " beats "  + capitalize(computerChoice) + "."
+    }
+}
+
+function play_rps_new(choice){
+
+    if (playerPoints < 5 && cpuPoints < 5){
+        playerChoice = choice
+        getComputerChoice();
+        return compareChoices_new(computerChoice,playerChoice.toLowerCase());
+    } else if (playerPoints === 5) {
+        score.textContent = `You won!`
+        result.textContent = ''
+        choiceButtons.style.display = 'none'
+        resetScoreBtn.style.display = 'inline'
+    } else {
+        score.textContent = `You lost!`
+        result.textContent = ''
+        choiceButtons.style.display = 'none'
+        resetScoreBtn.style.display = 'inline'
+    }
+}
 
 //Display results on the page instead of the console
 
 //Show a button to end the game
 
 //Show initial button selection after the game is finished
+
+
+//Basicamente hay que reimplementar todo porque lo de arriba esta mal
+//Hay que hacer que el juego empiece a correr recien cuando se escoje una opcion
+//Y no antes. Asì cuando elegis alguna opcion, la compare con la de la pc y te
+//Asigne puntos. El sistema de rondas no sirve en este caso, se maneja por puntuacion
+//Cuando llega a 5, ganas.
